fbdae354f8aba82fa9821da0de55ca77822ddcf5
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-04-23 20:38:10 -0500

    finished todo

---
+++ Todo.java
@@ -0,0 +1,36 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-04-23
+OU ID_______________: 113425757
+OU EMAIL____________: jake.r.jackson-1@ou.edu
+
+Tasks:
+1. finish setUpRegions
+2. finish paintComponent
+3. finish mouseClicked
+4. finish setSelectedRegionContents
+5. finish evaluate
+6. finish GraphicalCalculatorFrame
+7. test
+8. debug
+
+Estimated Time (minutes):
+1. 5
+2. 10
+3. 5
+4. 10
+5. 5
+6. 10
+7. 5
+8. 5
+
+Actual Time (minutes):
+1. 10
+2. 10
+3. 7
+4. 20
+5. 5
+6. 20
+7. 5
+8. 10
+*/}
